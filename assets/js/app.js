1ac224731ccb930cc7a110f4f6ccb379c488216b
Weng Fei Fung <weffung@ucdavis.edu>
2020-10-30 05:54:37 -0700

    Refactored: More readable function names

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,19 +31,19 @@ var TimeUtilities = {
     }
 }
 
-function loadTodaysDate() {
+function renderTodaysDate() {
     let humanReadableDate = TimeUtilities.getToday().humanReadableDate
     $("#currentDay").text(humanReadableDate);
 }
 
-function loadTimeblocks() {
+function renderTimeblocks() {
     function renderTimeblock(objectHours) {
         var template = $("#template-time-block").clone().html();
         var parameterizedTemplate = Handlebars.compile(template);
         var generatedHtml = parameterizedTemplate(objectHours);
         $(".time-blocks").append(generatedHtml);
     }
-    renderTimeblock({militaryHour:"5", regularHour:"5AM"});
+    renderTimeblock({militaryHour:"1", regularHour:"1AM"});
     renderTimeblock({militaryHour:"9", regularHour:"9AM"});
     renderTimeblock({militaryHour:"10", regularHour:"10AM"});
     renderTimeblock({militaryHour:"11", regularHour:"11AM"});
@@ -53,7 +53,8 @@ function loadTimeblocks() {
     renderTimeblock({militaryHour:"15", regularHour:"3PM"});
     renderTimeblock({militaryHour:"16", regularHour:"4PM"});
     renderTimeblock({militaryHour:"17", regularHour:"5PM"});
-}
+
+} // renderTimeblocks
 
 function colorTimeblocks() {
     $(".time-block").each( (i,timeBlock)=>{ 
@@ -68,7 +69,6 @@ function colorTimeblocks() {
         } else {
             $timeBlock.addClass("future");
         }
-        debugger;
      });
 }
 
