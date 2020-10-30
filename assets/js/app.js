be6c2a7be46293b4a2e8d41f48e51894c8708fba
Weng Fei Fung <weffung@ucdavis.edu>
2020-10-30 04:19:11 -0700

    Add todays date

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,27 @@
 console.log("JS Connected");
+
+/**
+ * 
+ * Utility methods for managing time
+ * 
+ * @object TimeUtilities
+ * @method  getToday
+ * @returns {object} object.unix            Unix Time
+ * @returns {object} object.humanReadable   Eg. Friday, October 30th, 2020
+ * 
+ * 
+ */
+var TimeUtilities = {
+    getToday: () => {
+        let now = moment.now();
+        return {
+            unixTime: moment(now).unix(),
+            humanReadableDate: moment(moment.now()).format("dddd, MMMM Do, YYYY")
+        }
+    }
+}
+
+function loadTodaysDate() {
+    let humanReadableDate = TimeUtilities.getToday().humanReadableDate
+    $("#currentDay").text(humanReadableDate);
+}
